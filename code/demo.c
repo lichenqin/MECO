cff28143dbd73917e50efbe257cbdd9f76649b9b
leec <1198133457@qq.com>
2020-06-18 00:32:05 +0800

    add parameters of mec in demo.c\nadd pso_E function

--- code/demo.c
+++ code/demo.c
@@ -25,6 +25,34 @@
 #include <stdio.h> // for printf
 #include "pso.h"
 
+//==============================================================
+//           Prameteres Of Mobile Edge Computing
+//==============================================================
+
+
+double Wu=pow(10,7);
+double Wd=10;
+double d=10;
+double h1=0.99;
+double h2=0.99;
+double N0=174*pow(10,-13);
+double P0=0.4;
+double kt=18;
+double Pr=0.4;
+double PF=0.1;
+double Ptmax=0.1;
+double fc=8*pow(10,8);
+double fImax=4*pow(10,8);
+double k=pow(10,-26);
+double alpha=40;
+double I=10*1024*1024*8;
+double v=4;
+double Lmax=4.5;
+double beta1=1;
+double beta2=0.2;
+double F=6*pow(10,9);
+double Ru=Wd*((log(1+(PF*pow(d,-v)*pow(abs(h2),2))/N0))/log(2));
+double Rd=Wd*((log(1+(PF*pow(d,-v)*pow(abs(h2),2))/N0))/log(2));
 
 //==============================================================
 //                  BENCHMARK FUNCTIONS
@@ -69,6 +97,52 @@ double pso_griewank(double *vec, int dim, void *params) {
 
 }
 
+//==============================================================
+//              Consuption Functions about Energe
+//==============================================================
+
+//公式4
+bool L(double *pos)
+{
+	double tl,tc,tu,td,tlc;
+	double fl=pos[0];
+    double Pt=pos[1];
+    double lamda=pos[2];
+    double Ru=Wu*((log(1+(Pt*pow(d,-v)*pow(abs(h2),2))/N0))/log(2));;
+    
+	tl = alpha*lamda*I/fl;
+    tu = beta1*(1 - lamda)*I/Ru;
+	td = beta2*(1 - lamda)*I/Rd;
+	tlc = alpha*(1 - lamda)*I/fc;
+	tc = tu + td + tlc;
+	if (tl <= Lmax && tc <= Lmax)
+		return true;
+	else
+		return false;
+}
+
+//公式11
+bool f(double *pos)
+{
+    double lamda=pos[2];
+    double f=I*(1-lamda)*alpha;
+	if (f <= F)
+		return true;
+	else
+		return false;
+}
+
+//耗能方程
+double pso_E(double *vec, int dim, void *params) {
+
+    double fl=vec[0];
+    double Pt=vec[1];
+    double lamda=vec[2];
+    double Ru=Wu*((log(1+(Pt*pow(d,-v)*pow(abs(h2),2))/N0))/log(2));;
+    double E = alpha*I*K*lamda*pow(fl,2)+(P0+kt*Pt)*(beta1*(1-lamda)*I)/Ru+Pr*beta2*(1-lamda)/Rd;
+
+    return E;
+}
 
 //==============================================================
 
